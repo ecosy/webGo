bb321187d6b35e65b16bf5f531496955833faafd
unknown <2918858@device.hmc.co.kr>
2021-01-11 15:22:36 +0900

    update: web05 go codes

---
+++ web05/go.mod
@@ -0,0 +1,3 @@
+module github.com/ecosy/webGo/tree/main/web05
+
+go 1.15
---
+++ web05/myapp/app.go
@@ -0,0 +1,9 @@
+package myapp
+
+import "net/http"
+
+// NewHandler make a new myapp handler
+func NewHandler() http.Handler {
+	mux := http.NewServeMux()
+	return mux
+}
